vishalbhoj/edk2-1

UEFI: Fastboot: Reduce HiKey max download size to avoid failures after chainloading from grub
When fastboot.efi is chainloaded from grub, we end up with not
enough contiguous memory to allocate 500M chunks. Sadly we also
can't allocate 256M chunks either (which is a bit silly).

None the less, this patch provides a workaround, limiting the
max size to 128M chunks. This does not cause flashing to take
more time, but does spit out more output when flashing the
multiple chunks.

Signed-off-by: John Stultz <john.stultz@linaro.org>

HisiPkg/HiKeyPkg/HiKey.dsc
@@ -344,8 +344,8 @@
 
   # Device path of block device on which Android Fastboot should flash partitions
   gHwTokenSpaceGuid.PcdAndroidFastbootNvmDevicePath|L"VenHw(b549f005-4bd4-4020-a0cb-06f42bda68c3)"
-  # Flash limit 500M/time, for memory concern
-  gHwTokenSpaceGuid.PcdArmFastbootFlashLimit|"524288000"
+  # Flash limit 128M/time, for memory concern
+  gHwTokenSpaceGuid.PcdArmFastbootFlashLimit|"134217728"
 
 ################################################################################
 #

vchong/OpenPlatformPkg

Platforms/Hisilicon/HiKey/HiKey.dsc
@@ -342,7 +342,8 @@
   gEmbeddedTokenSpaceGuid.PcdAndroidFastbootUsbVendorId|0x18d1
   gEmbeddedTokenSpaceGuid.PcdAndroidFastbootUsbProductId|0xd00d
   gHiKeyTokenSpaceGuid.PcdAndroidFastbootNvmDevicePath|L"VenHw(b549f005-4bd4-4020-a0cb-06f42bda68c3)"
-  gHiKeyTokenSpaceGuid.PcdArmFastbootFlashLimit|"524288000"
+  # Flash limit 128M/time, for memory concern
+  gHiKeyTokenSpaceGuid.PcdArmFastbootFlashLimit|"134217728"
 
   #
   # HiKey Boot Image