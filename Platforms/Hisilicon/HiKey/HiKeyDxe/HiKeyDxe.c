vchong/OpenPlatformPkg

commit 3aa3378206e7dc41d9bb77cc62068d89edfd1a28

HiKey: fix the truncated serial number string
Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

Platforms/Hisilicon/HiKey/HiKeyDxe/HiKeyDxe.c
@@ -29,7 +29,7 @@
 
 #include "HiKeyDxeInternal.h"
 
-#define SERIAL_NUMBER_LENGTH        16
+#define SERIAL_NUMBER_LENGTH        17
 #define SERIAL_NUMBER_LBA           1024
 #define SERIAL_NUMBER_BLOCK_SIZE    512
 #define RANDOM_MAGIC                0x9a4dbeaf
@@ -162,6 +162,7 @@ GetSerialNo (
     return Status;
   }
   CopyMem (SerialNo, DataUnicode, VariableSize);
+  *Length = VariableSize;
   return EFI_SUCCESS;
 }
 

commit f3311aeb57ef95cce038952e007872f1f6fe7e88

Fix abootimg support broken by ("fix the truncated serial number string")
The commit 3aa3378206e ("HiKey: fix the truncated serial number string")
broke abootimg support, rendering systems flashed with flashall.sh to
stop booting.

This change seems to make it work again.

Change-Id: I43495bc178175c0f4814b58a1c925f0fbf6c1b98
Signed-off-by: John Stultz <john.stultz@linaro.org>

Platforms/Hisilicon/HiKey/HiKeyDxe/HiKeyDxe.c
@@ -29,7 +29,7 @@
 
 #include "HiKeyDxeInternal.h"
 
-#define SERIAL_NUMBER_LENGTH        17
+#define SERIAL_NUMBER_LENGTH        16
 #define SERIAL_NUMBER_LBA           1024
 #define SERIAL_NUMBER_BLOCK_SIZE    512
 #define RANDOM_MAGIC                0x9a4dbeaf