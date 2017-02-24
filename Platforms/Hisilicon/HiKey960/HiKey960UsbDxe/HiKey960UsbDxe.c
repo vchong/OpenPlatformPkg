be2cbb54ab9fdf4cebade5e1ff928e0f98c914f1
z00210919 <z00210919@notesmail.huawei.com>
2017-02-24 15:58:23 +0800

    Platforms/Hisilicon/HiKey960: update on usb
    Reset USB OTG & PHY.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- Platforms/Hisilicon/HiKey960/HiKey960UsbDxe/HiKey960UsbDxe.c
+++ Platforms/Hisilicon/HiKey960/HiKey960UsbDxe/HiKey960UsbDxe.c
@@ -1,6 +1,6 @@
 /** @file
 *
-*  Copyright (c) 2015-2017, Linaro. All rights reserved.
+*  Copyright (c) 2017, Linaro. All rights reserved.
 *
 *  This program and the accompanying materials
 *  are licensed and made available under the terms and conditions of the BSD License
@@ -20,49 +20,20 @@
 #include <Library/UefiLib.h>
 #include <Library/UefiRuntimeServicesTableLib.h>
 
-#include <Protocol/EmbeddedGpio.h>
 #include <Protocol/DwUsb.h>
 
 #include <Hi3660.h>
 
+#define USB_EYE_PARAM                  0x01c466e3
+#define USB_PHY_TX_VBOOST_LVL          5
 
-#if 0
-#define USB_SEL_GPIO0_3          3     // GPIO 0_3
-#define USB_5V_HUB_EN            7     // GPIO 0_7
-#define USB_ID_DET_GPIO2_5       21    // GPIO 2_5
-#define USB_VBUS_DET_GPIO2_6     22    // GPIO 2_6
+#define DWC3_PHY_RX_OVRD_IN_HI         0x1006
+#define DWC3_PHY_RX_SCOPE_VDCC         0x1026
 
-// Jumper on pin5-6 of J15 determines whether boot to fastboot
-#define DETECT_J15_FASTBOOT      24    // GPIO 3_0
+#define RX_SCOPE_LFPS_EN               (1 << 0)
 
-STATIC EMBEDDED_GPIO *mGpio;
-
-STATIC
-VOID
-HiKey960DetectUsbModeInit (
-  IN VOID
-  )
-{
-  EFI_STATUS     Status;
-
-  /* set pullup on both GPIO2_5 & GPIO2_6. It's required for inupt. */
-  MmioWrite32 (0xf8001864, 1);
-  MmioWrite32 (0xf8001868, 1);
-
-  Status = gBS->LocateProtocol (&gEmbeddedGpioProtocolGuid, NULL, (VOID **)&mGpio);
-  ASSERT_EFI_ERROR (Status);
-  Status = mGpio->Set (mGpio, USB_SEL_GPIO0_3, GPIO_MODE_OUTPUT_0);
-  ASSERT_EFI_ERROR (Status);
-  Status = mGpio->Set (mGpio, USB_5V_HUB_EN, GPIO_MODE_OUTPUT_0);
-  ASSERT_EFI_ERROR (Status);
-  MicroSecondDelay (1000);
-
-  Status = mGpio->Set (mGpio, USB_ID_DET_GPIO2_5, GPIO_MODE_INPUT);
-  ASSERT_EFI_ERROR (Status);
-  Status = mGpio->Set (mGpio, USB_VBUS_DET_GPIO2_6, GPIO_MODE_INPUT);
-  ASSERT_EFI_ERROR (Status);
-}
-#endif
+#define TX_VBOOST_LVL_MASK             7
+#define TX_VBOOST_LVL(x)               ((x) & TX_VBOOST_LVL_MASK)
 
 UINTN
 HiKey960GetUsbMode (
@@ -72,71 +43,228 @@ HiKey960GetUsbMode (
   return USB_DEVICE_MODE;
 }
 
+VOID
+HiKey960UsbReset (
+  VOID
+  )
+{
+  UINT32               Data;
+
+  MmioWrite32 (CRG_REG_BASE + CRG_PERRSTEN4_OFFSET, PERRSTEN4_USB3OTG);
+  MmioWrite32 (CRG_REG_BASE + CRG_PERRSTEN4_OFFSET, PERRSTEN4_USB3OTGPHY_POR);
+  MmioWrite32 (
+    CRG_REG_BASE + CRG_PERRSTEN4_OFFSET,
+    PERRSTEN4_USB3OTG_MUX | PERRSTEN4_USB3OTG_AHBIF | PERRSTEN4_USB3OTG_32K
+    );
+  MmioWrite32 (
+    CRG_REG_BASE + CRG_PERDIS4_OFFSET,
+    PEREN4_GT_ACLK_USB3OTG | PEREN4_GT_CLK_USB3OTG_REF
+    );
+
+  Data = MmioRead32 (PCTRL_REG_BASE + PCTRL_CTRL24_OFFSET);
+  Data &= ~PCTRL_CTRL24_USB3PHY_3MUX1_SEL;
+  MmioWrite32 (PCTRL_REG_BASE + PCTRL_CTRL24_OFFSET, Data);
+
+  MmioWrite32 (PCTRL_REG_BASE + PCTRL_CTRL3_OFFSET, PCTRL_CTRL3_USB_TXCO_EN);
+  MicroSecondDelay (10000);
+}
+
+VOID
+HiKey960UsbPhyCrWaitAck (
+  VOID
+  )
+{
+  INT32                Timeout = 1000;
+  UINT32               Data;
+
+  while (TRUE) {
+    Data = MmioRead32 (USB3OTG_BC_REG_BASE + USB3OTG_PHY_CR_STS_OFFSET);
+    if ((Data & USB3OTG_PHY_CR_ACK) == USB3OTG_PHY_CR_ACK) {
+      return;
+    }
+    MicroSecondDelay (50);
+    if (Timeout-- <= 0) {
+      DEBUG ((DEBUG_ERROR, "Wait PHY_CR_ACK timeout!\n"));
+      return;
+    }
+  }
+}
+
+VOID
+HiKey960UsbPhyCrSetAddr (
+  IN UINT32            Addr
+  )
+{
+  UINT32               Data;
+
+  MmioWrite32 (
+    USB3OTG_BC_REG_BASE + USB3OTG_PHY_CR_CTRL_OFFSET,
+    USB3OTG_PHY_CR_DATA_IN (Addr)
+    );
+  MicroSecondDelay (100);
+  Data = MmioRead32 (USB3OTG_BC_REG_BASE + USB3OTG_PHY_CR_CTRL_OFFSET);
+  Data |= USB3OTG_PHY_CR_CAP_ADDR;
+  MmioWrite32 (USB3OTG_BC_REG_BASE + USB3OTG_PHY_CR_CTRL_OFFSET, Data);
+  HiKey960UsbPhyCrWaitAck ();
+  MmioWrite32 (USB3OTG_BC_REG_BASE + USB3OTG_PHY_CR_CTRL_OFFSET, 0);
+}
+
+UINT16
+HiKey960UsbPhyCrRead (
+  IN UINT32            Addr
+  )
+{
+  INT32                Timeout = 1000;
+  UINT32               Data;
+
+  HiKey960UsbPhyCrSetAddr (Addr);
+  MmioWrite32 (
+    USB3OTG_BC_REG_BASE + USB3OTG_PHY_CR_CTRL_OFFSET,
+    USB3OTG_PHY_CR_READ
+    );
+  MicroSecondDelay (100);
+
+  while (TRUE) {
+    Data = MmioRead32 (USB3OTG_BC_REG_BASE + USB3OTG_PHY_CR_STS_OFFSET);
+    if ((Data & USB3OTG_PHY_CR_ACK) == USB3OTG_PHY_CR_ACK) {
+      DEBUG ((
+        DEBUG_INFO,
+        "Addr 0x%x, Data Out:0x%x\n",
+        Addr,
+        USB3OTG_PHY_CR_DATA_OUT(Data)
+        ));
+      break;
+    }
+    MicroSecondDelay (50);
+    if (Timeout-- <= 0) {
+      DEBUG ((DEBUG_ERROR, "Wait PHY_CR_ACK timeout!\n"));
+      break;
+    }
+  }
+  MmioWrite32 (USB3OTG_BC_REG_BASE + USB3OTG_PHY_CR_CTRL_OFFSET, 0);
+  return (UINT16)USB3OTG_PHY_CR_DATA_OUT(Data);
+}
+
+VOID
+HiKey960UsbPhyCrWrite (
+  IN UINT32            Addr,
+  IN UINT32            Value
+  )
+{
+  UINT32               Data;
+
+  HiKey960UsbPhyCrSetAddr (Addr);
+  Data = USB3OTG_PHY_CR_DATA_IN(Value);
+  MmioWrite32 (USB3OTG_BC_REG_BASE + USB3OTG_PHY_CR_CTRL_OFFSET, Data);
+
+  Data = MmioRead32 (USB3OTG_BC_REG_BASE + USB3OTG_PHY_CR_CTRL_OFFSET);
+  Data |= USB3OTG_PHY_CR_CAP_DATA;
+  MmioWrite32 (USB3OTG_BC_REG_BASE + USB3OTG_PHY_CR_CTRL_OFFSET, Data);
+  HiKey960UsbPhyCrWaitAck ();
+
+  MmioWrite32 (USB3OTG_BC_REG_BASE + USB3OTG_PHY_CR_CTRL_OFFSET, 0);
+  MmioWrite32 (
+    USB3OTG_BC_REG_BASE + USB3OTG_PHY_CR_CTRL_OFFSET,
+    USB3OTG_PHY_CR_WRITE
+    );
+  HiKey960UsbPhyCrWaitAck ();
+}
+
+VOID
+HiKey960UsbSetEyeDiagramParam (
+  VOID
+  )
+{
+  UINT32               Data;
+
+  /* set eye diagram for usb 2.0 */
+  MmioWrite32 (USB3OTG_BC_REG_BASE + USB3OTG_CTRL4_OFFSET, USB_EYE_PARAM);
+  /* set eye diagram for usb 3.0 */
+  HiKey960UsbPhyCrRead (DWC3_PHY_RX_OVRD_IN_HI);
+  HiKey960UsbPhyCrWrite (DWC3_PHY_RX_OVRD_IN_HI, BIT11 | BIT9 | BIT8 |BIT7);
+  HiKey960UsbPhyCrRead (DWC3_PHY_RX_OVRD_IN_HI);
+
+  /* enable RX_SCOPE_LFPS_EN for usb 3.0 */
+  Data = HiKey960UsbPhyCrRead (DWC3_PHY_RX_SCOPE_VDCC);
+  Data |= RX_SCOPE_LFPS_EN;
+  HiKey960UsbPhyCrWrite (DWC3_PHY_RX_SCOPE_VDCC, Data);
+  HiKey960UsbPhyCrRead (DWC3_PHY_RX_SCOPE_VDCC);
+
+  Data = MmioRead32 (USB3OTG_BC_REG_BASE + USB3OTG_CTRL6_OFFSET);
+  Data &= ~TX_VBOOST_LVL_MASK;
+  Data = TX_VBOOST_LVL (USB_PHY_TX_VBOOST_LVL);
+  MmioWrite32 (USB3OTG_BC_REG_BASE + USB3OTG_CTRL6_OFFSET, Data);
+  DEBUG ((
+    DEBUG_INFO,
+    "set ss phy tx vboost lvl 0x%x\n",
+    MmioRead32 (USB3OTG_BC_REG_BASE + USB3OTG_CTRL6_OFFSET)
+    ));
+}
+
+VOID
+HiKey960UsbRelease (
+  VOID
+  )
+{
+  UINT32               Data;
+
+  /* enable USB REFCLK ISO */
+  MmioWrite32 (CRG_REG_BASE + CRG_ISODIS_OFFSET, PERISOEN_USB_REFCLK_ISO_EN);
+  /* enable USB_TXCO_EN */
+  MmioWrite32 (PCTRL_CTRL3_OFFSET, PCTRL_CTRL3_USB_TXCO_EN);
+
+  Data = MmioRead32 (PCTRL_REG_BASE + PCTRL_CTRL24_OFFSET);
+  Data &= ~PCTRL_CTRL24_USB3PHY_3MUX1_SEL;
+  MmioWrite32 (PCTRL_REG_BASE + PCTRL_CTRL24_OFFSET, Data);
+
+  MmioWrite32 (
+    CRG_REG_BASE + CRG_PEREN4_OFFSET,
+    PEREN4_GT_ACLK_USB3OTG | PEREN4_GT_CLK_USB3OTG_REF
+    );
+  MmioWrite32 (
+    CRG_REG_BASE + CRG_PERRSTDIS4_OFFSET,
+    PERRSTEN4_USB3OTG_MUX | PERRSTEN4_USB3OTG_AHBIF | PERRSTEN4_USB3OTG_32K
+    );
+
+  MmioWrite32 (
+    CRG_REG_BASE + CRG_PERRSTEN4_OFFSET,
+    PERRSTEN4_USB3OTG | PERRSTEN4_USB3OTGPHY_POR
+    );
+
+  /* enable PHY REF CLK */
+  Data = MmioRead32 (USB3OTG_BC_REG_BASE + USB3OTG_CTRL0_OFFSET);
+  Data |= USB3OTG_CTRL0_SC_USB3PHY_ABB_GT_EN;
+  MmioWrite32 (USB3OTG_BC_REG_BASE + USB3OTG_CTRL0_OFFSET, Data);
+
+  Data = MmioRead32 (USB3OTG_BC_REG_BASE + USB3OTG_CTRL7_OFFSET);
+  Data |= USB3OTG_CTRL7_REF_SSP_EN;
+  MmioWrite32 (USB3OTG_BC_REG_BASE + USB3OTG_CTRL7_OFFSET, Data);
+
+  /* exit from IDDQ mode */
+  Data = MmioRead32 (USB3OTG_BC_REG_BASE + USB3OTG_CTRL2_OFFSET);
+  Data &= ~(USB3OTG_CTRL2_TEST_POWERDOWN_SSP | USB3OTG_CTRL2_TEST_POWERDOWN_HSP);
+  MmioWrite32 (USB3OTG_BC_REG_BASE + USB3OTG_CTRL2_OFFSET, Data);
+  MicroSecondDelay (100000);
+
+  MmioWrite32 (CRG_REG_BASE + CRG_PERRSTDIS4_OFFSET, PERRSTEN4_USB3OTGPHY_POR);
+  MmioWrite32 (CRG_REG_BASE + CRG_PERRSTDIS4_OFFSET, PERRSTEN4_USB3OTG);
+  MicroSecondDelay (10000);
+  Data = MmioRead32 (USB3OTG_BC_REG_BASE + USB3OTG_CTRL3_OFFSET);
+  Data |= USB3OTG_CTRL3_VBUSVLDEXT | USB3OTG_CTRL3_VBUSVLDEXTSEL;
+  MmioWrite32 (USB3OTG_BC_REG_BASE, Data);
+  MicroSecondDelay (100000);
+
+  HiKey960UsbSetEyeDiagramParam ();
+}
+
 EFI_STATUS
 HiKey960UsbPhyInit (
   IN UINT8        Mode
   )
 {
-#if 0
-  UINTN         Value;
-  UINT32        Data;
-
-  HiKey960DetectUsbModeInit ();
-
-  //setup clock
-  MmioWrite32 (PERI_CTRL_BASE + SC_PERIPH_CLKEN0, BIT4);
-  do {
-       Value = MmioRead32 (PERI_CTRL_BASE + SC_PERIPH_CLKSTAT0);
-  } while ((Value & BIT4) == 0);
-
-  //setup phy
-  Data = RST0_USBOTG_BUS | RST0_POR_PICOPHY |
-           RST0_USBOTG | RST0_USBOTG_32K;
-  MmioWrite32 (PERI_CTRL_BASE + SC_PERIPH_RSTDIS0, Data);
-  do {
-    Value = MmioRead32 (PERI_CTRL_BASE + SC_PERIPH_RSTSTAT0);
-    Value &= Data;
-  } while (Value);
-
-  Value = MmioRead32 (PERI_CTRL_BASE + SC_PERIPH_CTRL4);
-  Value &= ~(CTRL4_PICO_SIDDQ | CTRL4_FPGA_EXT_PHY_SEL |
-             CTRL4_OTG_PHY_SEL);
-  Value |=  CTRL4_PICO_VBUSVLDEXT | CTRL4_PICO_VBUSVLDEXTSEL;
-  MmioWrite32 (PERI_CTRL_BASE + SC_PERIPH_CTRL4, Value);
-  MicroSecondDelay (1000);
-
-  //If Mode = 1, USB in Device Mode
-  //If Mode = 0, USB in Host Mode
-  if (Mode == USB_DEVICE_MODE) {
-    if (HiKey960GetUsbMode () == USB_DEVICE_MODE) {
-      DEBUG ((DEBUG_ERROR, "usb work as device mode.\n"));
-    } else {
-      return EFI_INVALID_PARAMETER;
-    }
-
-     Value = MmioRead32 (PERI_CTRL_BASE + SC_PERIPH_CTRL5);
-     Value &= ~CTRL5_PICOPHY_BC_MODE;
-     MmioWrite32 (PERI_CTRL_BASE + SC_PERIPH_CTRL5, Value);
-     MicroSecondDelay (20000);
-  } else {
-    if (HiKey960GetUsbMode () == USB_HOST_MODE) {
-      DEBUG ((DEBUG_ERROR, "usb work as host mode.\n"));
-    } else {
-      return EFI_INVALID_PARAMETER;
-    }
-
-    /*CTRL5*/
-    Data = MmioRead32 (PERI_CTRL_BASE + SC_PERIPH_CTRL5);
-    Data &= ~CTRL5_PICOPHY_BC_MODE;
-    Data |= CTRL5_USBOTG_RES_SEL | CTRL5_PICOPHY_ACAENB |
-            CTRL5_PICOPHY_VDATDETENB | CTRL5_PICOPHY_DCDENB;
-    MmioWrite32 (PERI_CTRL_BASE + SC_PERIPH_CTRL5, Data);
-    MicroSecondDelay (20000);
-    MmioWrite32 (PERI_CTRL_BASE + 0x018, 0x70533483); //EYE_PATTERN
-
-    MicroSecondDelay (5000);
-  }
-#endif
+  HiKey960UsbReset ();
+  MicroSecondDelay (10000);
+  HiKey960UsbRelease ();
 
   return EFI_SUCCESS;
 }
--- Platforms/Hisilicon/HiKey960/Include/Hi3660.h
+++ Platforms/Hisilicon/HiKey960/Include/Hi3660.h
@@ -15,14 +15,67 @@
 #ifndef __HI3660_H__
 #define __HI3660_H__
 
+#define PCTRL_REG_BASE                          0xE8A09000
+
+#define PCTRL_CTRL3_OFFSET                      0x010
+#define PCTRL_CTRL24_OFFSET                     0x064
+
+#define PCTRL_CTRL3_USB_TXCO_EN                 (1 << 1)
+#define PCTRL_CTRL24_USB3PHY_3MUX1_SEL          (1 << 25)
+
+#define USB3OTG_BC_REG_BASE                     0xFF200000
+
+#define USB3OTG_CTRL0_OFFSET                    0x000
+#define USB3OTG_CTRL2_OFFSET                    0x008
+#define USB3OTG_CTRL3_OFFSET                    0x00C
+#define USB3OTG_CTRL4_OFFSET                    0x010
+#define USB3OTG_CTRL6_OFFSET                    0x018
+#define USB3OTG_CTRL7_OFFSET                    0x01C
+#define USB3OTG_PHY_CR_STS_OFFSET               0x050
+#define USB3OTG_PHY_CR_CTRL_OFFSET              0x054
+
+#define USB3OTG_CTRL0_SC_USB3PHY_ABB_GT_EN      (1 << 15)
+#define USB3OTG_CTRL2_TEST_POWERDOWN_SSP        (1 << 1)
+#define USB3OTG_CTRL2_TEST_POWERDOWN_HSP        (1 << 0)
+#define USB3OTG_CTRL3_VBUSVLDEXT                (1 << 6)
+#define USB3OTG_CTRL3_VBUSVLDEXTSEL             (1 << 5)
+#define USB3OTG_CTRL7_REF_SSP_EN                (1 << 16)
+#define USB3OTG_PHY_CR_DATA_OUT(x)              (((x) & 0xFFFF) << 1)
+#define USB3OTG_PHY_CR_ACK                      (1 << 0)
+#define USB3OTG_PHY_CR_DATA_IN(x)               (((x) & 0xFFFF) << 4)
+#define USB3OTG_PHY_CR_WRITE                    (1 << 3)
+#define USB3OTG_PHY_CR_READ                     (1 << 2)
+#define USB3OTG_PHY_CR_CAP_DATA                 (1 << 1)
+#define USB3OTG_PHY_CR_CAP_ADDR                 (1 << 0)
+
 #define CRG_REG_BASE                            0xFFF35000
 
+#define CRG_PEREN4_OFFSET                       0x040
+#define CRG_PERDIS4_OFFSET                      0x044
+#define CRG_PERCLKEN4_OFFSET                    0x048
 #define CRG_PERRSTEN3_OFFSET                    0x084
 #define CRG_PERRSTDIS3_OFFSET                   0x088
 #define CRG_PERRSTSTAT3_OFFSET                  0x08C
+#define CRG_PERRSTEN4_OFFSET                    0x090
+#define CRG_PERRSTDIS4_OFFSET                   0x094
+#define CRG_PERRSTSTAT4_OFFSET                  0x098
+#define CRG_ISOEN_OFFSET                        0x144
+#define CRG_ISODIS_OFFSET                       0x148
+#define CRG_ISOSTAT_OFFSET                      0x14C
 
 #define PERI_UFS_BIT                            (1 << 12)
 #define PERI_ARST_UFS_BIT                       (1 << 7)
+
+#define PEREN4_GT_ACLK_USB3OTG                  (1 << 1)
+#define PEREN4_GT_CLK_USB3OTG_REF               (1 << 0)
+
+#define PERRSTEN4_USB3OTG_MUX                   (1 << 8)
+#define PERRSTEN4_USB3OTG_AHBIF                 (1 << 7)
+#define PERRSTEN4_USB3OTG_32K                   (1 << 6)
+#define PERRSTEN4_USB3OTG                       (1 << 5)
+#define PERRSTEN4_USB3OTGPHY_POR                (1 << 3)
+
+#define PERISOEN_USB_REFCLK_ISO_EN              (1 << 25)
 
 #define CRG_CLKDIV16_OFFSET                     0x0E8
 #define SC_DIV_UFSPHY_CFG_MASK                  (0x3 << 9)
