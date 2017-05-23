317efc8dbf54d0d076df15eea29fb7dec8454c40
Haojian Zhuang <haojian.zhuang@linaro.org>
2017-05-23 13:35:31 +0800

    Platforms/HiKeyDxe: load serialno into cmdline
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- Platforms/Hisilicon/HiKey/HiKeyDxe/HiKeyDxe.c
+++ Platforms/Hisilicon/HiKey/HiKeyDxe/HiKeyDxe.c
@@ -15,16 +15,34 @@
 
 #include <Library/BaseMemoryLib.h>
 #include <Library/CacheMaintenanceLib.h>
+#include <Library/DebugLib.h>
 #include <Library/DevicePathLib.h>
 #include <Library/IoLib.h>
 #include <Library/MemoryAllocationLib.h>
+#include <Library/PrintLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
 #include <Library/UefiRuntimeServicesTableLib.h>
 
+#include <Protocol/Abootimg.h>
+#include <Protocol/BlockIo.h>
+
 #include <Hi6220.h>
+#include <libfdt.h>
 
 #include "Hi6220RegsPeri.h"
+
+#define SERIAL_NUMBER_SIZE               17
+#define SERIAL_NUMBER_BLOCK_SIZE         EFI_PAGE_SIZE
+#define SERIAL_NUMBER_LBA                1024
+#define RANDOM_MAX                       0x7FFFFFFFFFFFFFFF
+#define RANDOM_MAGIC                     0x9A4DBEAF
+
+typedef struct {
+  UINT64        Magic;
+  UINT64        Data;
+  CHAR16        UnicodeSN[SERIAL_NUMBER_SIZE];
+} RANDOM_SERIAL_NUMBER;
 
 STATIC
 VOID
@@ -94,10 +112,148 @@ HiKeyInitPeripherals (
 
 EFI_STATUS
 EFIAPI
+AbootimgAppendKernelArgs (
+  IN CHAR16            *Args,
+  IN UINTN              Size
+  )
+{
+  EFI_STATUS                  Status;
+  EFI_BLOCK_IO_PROTOCOL      *BlockIoProtocol;
+  VOID                       *DataPtr;
+  RANDOM_SERIAL_NUMBER       *RandomSN;
+  EFI_DEVICE_PATH_PROTOCOL   *FlashDevicePath;
+  EFI_HANDLE                  FlashHandle;
+
+  if (Args == NULL) {
+    return EFI_INVALID_PARAMETER;
+  }
+  FlashDevicePath = ConvertTextToDevicePath ((CHAR16*)FixedPcdGetPtr (PcdAndroidFastbootNvmDevicePath));
+  Status = gBS->LocateDevicePath (&gEfiBlockIoProtocolGuid, &FlashDevicePath, &FlashHandle);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "Warning: Couldn't locate Android NVM device (status: %r)\n", Status));
+    // Failing to locate partitions should not prevent to do other Android FastBoot actions
+    return EFI_SUCCESS;
+  }
+  Status = gBS->OpenProtocol (
+                  FlashHandle,
+                  &gEfiBlockIoProtocolGuid,
+                  (VOID **) &BlockIoProtocol,
+                  gImageHandle,
+                  NULL,
+                  EFI_OPEN_PROTOCOL_GET_PROTOCOL
+                  );
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_WARN, "Warning: Couldn't open block device (status: %r)\n", Status));
+    return EFI_DEVICE_ERROR;
+  }
+
+  DataPtr = AllocatePages (1);
+  if (DataPtr == NULL) {
+    return EFI_BUFFER_TOO_SMALL;
+  }
+  Status = BlockIoProtocol->ReadBlocks (
+                              BlockIoProtocol,
+                              BlockIoProtocol->Media->MediaId,
+                              SERIAL_NUMBER_LBA,
+                              SERIAL_NUMBER_BLOCK_SIZE,
+                              DataPtr
+                              );
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_WARN, "Warning: Failed on reading blocks\n"));
+    goto Exit;
+  }
+  RandomSN = (RANDOM_SERIAL_NUMBER *)DataPtr;
+  if (RandomSN->Magic != RANDOM_MAGIC) {
+    UnicodeSPrint(
+      RandomSN->UnicodeSN, SERIAL_NUMBER_SIZE * sizeof (CHAR16),
+      L"0123456789abcdef"
+      );
+  }
+  UnicodeSPrint (
+    Args + StrLen (Args), Size - StrLen (Args),
+    L" androidboot.serialno=%s",
+    RandomSN->UnicodeSN
+    );
+  FreePages (DataPtr, 1);
+  return EFI_SUCCESS;
+Exit:
+  FreePages (DataPtr, 1);
+  return Status;
+}
+
+EFI_STATUS
+EFIAPI
+AbootimgUpdateDtb (
+  IN  EFI_PHYSICAL_ADDRESS        OrigFdtBase,
+  OUT EFI_PHYSICAL_ADDRESS       *NewFdtBase
+  )
+{
+  UINTN             FdtSize, NumPages;
+  INTN              err;
+  EFI_STATUS        Status;
+
+  //
+  // Sanity checks on the original FDT blob.
+  //
+  err = fdt_check_header ((VOID*)(UINTN)OrigFdtBase);
+  if (err != 0) {
+    DEBUG ((DEBUG_ERROR, "ERROR: Device Tree header not valid (err:%d)\n", err));
+    return EFI_INVALID_PARAMETER;
+  }
+
+  //
+  // Store the FDT as Runtime Service Data to prevent the Kernel from
+  // overwritting its data.
+  //
+  FdtSize = fdt_totalsize ((VOID *)(UINTN)OrigFdtBase);
+  NumPages = EFI_SIZE_TO_PAGES (FdtSize) + 20;
+  Status = gBS->AllocatePages (
+                  AllocateAnyPages, EfiRuntimeServicesData,
+                  NumPages, NewFdtBase);
+  if (EFI_ERROR (Status)) {
+    return EFI_BUFFER_TOO_SMALL;
+  }
+
+  CopyMem (
+    (VOID*)(UINTN)*NewFdtBase,
+    (VOID*)(UINTN)OrigFdtBase,
+    FdtSize
+    );
+
+  fdt_pack ((VOID*)(UINTN)*NewFdtBase);
+  err = fdt_check_header ((VOID*)(UINTN)*NewFdtBase);
+  if (err != 0) {
+    DEBUG ((DEBUG_ERROR, "ERROR: Device Tree header not valid (err:%d)\n", err));
+    gBS->FreePages (*NewFdtBase, NumPages);
+    return EFI_INVALID_PARAMETER;
+  }
+  return EFI_SUCCESS;
+}
+
+ABOOTIMG_PROTOCOL mAbootimg = {
+  AbootimgAppendKernelArgs,
+  AbootimgUpdateDtb
+};
+
+EFI_STATUS
+EFIAPI
 HiKeyEntryPoint (
   IN EFI_HANDLE         ImageHandle,
   IN EFI_SYSTEM_TABLE   *SystemTable
   )
 {
-  return HiKeyInitPeripherals ();
+  EFI_STATUS            Status;
+
+  Status = HiKeyInitPeripherals ();
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  Status = gBS->InstallProtocolInterface (
+                  &ImageHandle,
+                  &gAbootimgProtocolGuid,
+                  EFI_NATIVE_INTERFACE,
+                  &mAbootimg
+                  );
+  return Status;
 }
